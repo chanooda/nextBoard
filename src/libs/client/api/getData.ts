a6ee326ab9c51c4fd43b8f9b43cb392c2d0e0953
chanooda <hanrhfqkq@gmail.com>
2022-07-17 11:51:14 +0900

    마무리

--- src/libs/client/api/getData.ts
+++ src/libs/client/api/getData.ts
@@ -1,17 +1,23 @@
 export const getBoards = async () => {
-  const response = await fetch("/api/board");
+  const response = await fetch(
+    "https://nextboard-6d5snyfgu-hanrhfqkq-gmailcom.vercel.app/api/board"
+  );
   const data = await response.json();
   return data.boards;
 };
 
 export const getBoard = async (id: number) => {
-  const response = await fetch(`/api/board?id=${id}`);
+  const response = await fetch(
+    `https://nextboard-6d5snyfgu-hanrhfqkq-gmailcom.vercel.app/api/board?id=${id}`
+  );
   const data = await response.json();
   return data.boards[0];
 };
 
 export const getPost = async (id: number) => {
-  const response = await fetch(`/api/post?id=${id}`);
+  const response = await fetch(
+    `https://nextboard-6d5snyfgu-hanrhfqkq-gmailcom.vercel.app/api/post?id=${id}`
+  );
   const data = await response.json();
   return data.posts[0];
 };
