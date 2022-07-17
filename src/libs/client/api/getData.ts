a7828ce20476ae30f258ef18dc06c0c4b48a5e9d
chanooda <hanrhfqkq@gmail.com>
2022-07-17 12:27:00 +0900

    asd

--- src/libs/client/api/getData.ts
+++ src/libs/client/api/getData.ts
@@ -1,23 +1,17 @@
 export const getBoards = async () => {
-  const response = await fetch(
-    "https://nextboard-dglb4b6yk-hanrhfqkq-gmailcom.vercel.app/api/board"
-  );
+  const response = await fetch("https://next-board.herokuapp.com/api/board");
   const data = await response.json();
   return data.boards;
 };
 
 export const getBoard = async (id: number) => {
-  const response = await fetch(
-    `https://nextboard-dglb4b6yk-hanrhfqkq-gmailcom.vercel.app/api/board?id=${id}`
-  );
+  const response = await fetch(`https://next-board.herokuapp.com/api/board?id=${id}`);
   const data = await response.json();
   return data.boards[0];
 };
 
 export const getPost = async (id: number) => {
-  const response = await fetch(
-    `https://nextboard-dglb4b6yk-hanrhfqkq-gmailcom.vercel.app/api/post?id=${id}`
-  );
+  const response = await fetch(`https://next-board.herokuapp.com/api/post?id=${id}`);
   const data = await response.json();
   return data.posts[0];
 };
